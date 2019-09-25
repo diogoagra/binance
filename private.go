e308575188b3d0ed64df339b9c9a2235e2a1f26a
Diogo Agra <diogoagra@gmail.com>
2019-09-24 22:00:36 -0300

    brioca

--- private.go
+++ private.go
@@ -22,7 +22,7 @@ func (b *Binance) OrderTest(symbol, side string, quantity, price float64) {
 // OrderMarket -
 func (b *Binance) OrderMarket(symbol, side string, quantity float64) {
 	timestamp := strconv.Itoa(int(unixMillis(time.Now())))
-	parameters := fmt.Sprintf("symbol=%s&side=%s&type=MARKET&quantity=%.8f&recvWindow=5000&timestamp=%s", symbol, side, quantity, timestamp)
+	parameters := fmt.Sprintf("symbol=%s&side=%s&type=MARKET&quantity=%.6f&recvWindow=5000&timestamp=%s", symbol, side, quantity, timestamp)
 	response, err := b.GetURL("/api/v3/order?"+fmt.Sprintf("%s&signature=%s", parameters, b.Sign(parameters)), "POST", true)
 	if err != nil {
 		log.Println(err)
